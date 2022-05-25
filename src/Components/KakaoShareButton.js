ad48e8c01d51c1ce09bab7c8ea004806b59adc52
dldmswl090 <79970323+dldmswl090@users.noreply.github.com>
2022-05-25 16:44:22 +0900

    add kakao other

--- src/Components/KakaoShareButton.js
+++ src/Components/KakaoShareButton.js
@@ -8,18 +8,18 @@ const KakaoShareButton = () => {
     //공유하고 싶은 우리 결과 URL 담을 변수
     const resultUrl = window.location.href;
 
-    //console.log(resultUrl,url) 이렇게 찍으면 안나옴
+    console.log(resultUrl,url) 
     // public안에 _redirects라는 폴더 하나 만들어서 그 안에 /* /index.html 200 이라고 넣어주고 저장하기
 
     React.useEffect(()=>{
         //카카오 인증이 계속 되는것을 막아주기 위한 역할?
-        Kakao.clearup();
+        Kakao.cleanup();
         Kakao.init('1932de940a9405ce799b75f1be5bc27d');
-        console.log(Kakao.isInitialized())
+        console.log(Kakao.isInitialized());
     },[])
 
     //카카오톡 공유하기 눌렀을때 공유하는 함수를 또 만들어서 밑에 애들을 감싸줘야함
-    const shareKakao = () => {
+const shareKakao = () => {
     Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
